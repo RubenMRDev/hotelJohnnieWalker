1f5c1ae004f809152cbeaf9e27a46579fc2a790a
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-05 12:07:36 +0100

    map changes

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Map = () => {
   return (
-    <div 
+    <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        width: '100%',}}
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+      }}
     >
-      <MapContainer 
-        center={[36.510034987389005, -4.8861989382417566]} 
-        zoom={16} 
-        style={{ height: '45vh', width: '65%', border: '4px solid #A9A9A9', borderRadius: '30px' }}
+      <MapContainer
+        center={[36.510034987389005, -4.8861989382417566]}
+        zoom={16}
+        style={{
+          height: "45vh",
+          width: "65%",
+          border: "4px solid #A9A9A9",
+          borderRadius: "30px",
+        }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={[36.510034987389005, -4.8861989382417566]}>
-          <Popup>Marbella</Popup>
+          <Popup>C. Pedraza, 12, 29601 Marbella, Málaga</Popup>
         </Marker>
       </MapContainer>
     </div>
