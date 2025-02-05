98141534d9cbf3d4e623232ac58d7304094c6742
RubenMRDev <rubenmartinruiz05@gmail.com>
2025-02-05 09:04:50 +0100

    Changed name to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/Register.jsx';
-import Main from './pages/Main.jsx';
-import Profile from './pages/profile.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import ReserveHotel from './pages/ReserveHotel.jsx';
-import ReserveRestaurant from './pages/ReserveRestaurant.jsx';
+import LandingPage from './routes/Register.jsx';
+import Main from './routes/Main.js';
+import Profile from './routes/profile.js';
+import Login from './routes/Login.js';
+import Register from './routes/Register.jsx';
+import ReserveHotel from './routes/ReserveHotel.js';
+import ReserveRestaurant from './routes/ReserveRestaurant.jsx';
 
 const App = () => {
   return (
