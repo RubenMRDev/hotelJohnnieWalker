1331ad2985d82c3c171c82417fdb72295ae80cf3
JairoAlexandro <jairosf3113@gmail.com>
2025-02-13 09:53:29 +0100

    resolved problems with tailwind

--- src/components/LoginRegister/LoginRegisterForm.jsx
+++ src/components/LoginRegister/LoginRegisterForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
-import "./LoginRegister.css";
 
 const LoginRegister = () => {
     const [isLogin, setIsLogin] = useState(true);
