446b3041ba9aec5f68e0162909c1619fd4b0923d
JairoAlexandro <jairosf3113@gmail.com>
2025-02-13 09:19:04 +0100

    Add MainHotel component and integrate it into Main page

---
+++ src/components/MainHotel/MainHotel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'MainHotel.css';
+
+const MainHotel = () => {
+    return(
+        <div className="flex justify-center items-center w-full bg-cover bg-center p-4" style={{ backgroundImage: "url('../src/assets/image/bg-restaurant.png')"}}>
+
+        </div>
+    );
+};
+
+export default MainHotel;
