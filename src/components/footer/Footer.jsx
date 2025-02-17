0886c23b5beb3ef78860ca95b9a9673b63378a5d
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-17 10:44:27 +0100

    Routes changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <p className="hotel-name">&copy; Hotel Johnnie Walker</p>
+        <p className="hotel-name">&copy; {new Date().getFullYear()} Hotel Johnnie Walker</p>
         <a href="https://github.com/RubenMRDev/hotelJohnnieWalker" target="_blank">
-          <img className="image" src="src/assets/images/github.png" alt="Github logo"></img>
+          <img className="image" src="/src/assets/images/github.png" alt="Github logo"></img>
         </a>
       </div>
     </footer>
---
+++ src/components/footer/Footer.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Footer from './Footer';
+import { describe, it, expect } from 'vitest';
+
+describe('Footer', () => {
+  it('should render the hotel name', () => {
+    render(<Footer />);
+    const hotelName = screen.getByText(/Hotel Johnnie Walker/i);
+    expect(hotelName).toBeInTheDocument();
+  });
+
+  it('should render the correct year', () => {
+    const currentYear = new Date().getFullYear();
+    render(<Footer />);
+    const yearElement = screen.getByText(new RegExp(currentYear.toString(), 'i'));
+    expect(yearElement).toBeInTheDocument();
+  });
+
+  it('should render the github logo', () => {
+    render(<Footer />);
+    const logoElement = screen.getByAltText(/Github logo/i);
+    expect(logoElement).toBeInTheDocument();
+  });
+});
