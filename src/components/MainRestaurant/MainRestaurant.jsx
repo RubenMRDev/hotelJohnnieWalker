7f103300a204be2f979daa715aeea85b535e6969
JairoAlexandro <jairosf3113@gmail.com>
2025-02-17 11:14:55 +0100

    Update MainHotel and MainRestaurant components to improve heading structure

--- src/components/MainRestaurant/MainRestaurant.jsx
+++ src/components/MainRestaurant/MainRestaurant.jsx
@@ -7,8 +7,11 @@ return (
         style={{ backgroundImage: `url('../src/assets/image/mainRestaurant.jpg')` }}
     >
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 text-center">
-            JOHNNIE WALKER<br className="hidden md:inline-block" />RESTAURANTE
+            JOHNNIE WALKER
         </h1>
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6 text-center">
+            RESTAURANTE
+        </h2>
         <button className="bg-[#D9B26A] text-black px-9 py-3 text-lg font-semibold rounded-lg shadow-md hover:bg-yellow-600 transition">
             Reservar
         </button>
