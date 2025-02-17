85447be8be88303cdf78285d7b05d8e2bff92750
RubenMRDev <rubenmartinruiz05@gmail.com>
2025-02-17 08:56:09 +0100

    Fixed review component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
-import LoginRegister from "../components/LoginRegister/LoginRegisterForm";
+import Review from "../components/Review/Review.jsx";
 
 
 function LandingPage() {
   return (
-    <div className="">
+    <div>
+      <Review nombre="John Doe" fecha="2023-10-12" calificacion={4.5} texto="Great experience, highly recommended!" />
     </div>
   );
 }
