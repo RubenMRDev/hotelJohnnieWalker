cf89923a6a71090d6fad844d68efe5b362c18cec
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-19 09:48:02 +0100

    map fixed

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,30 +1,23 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "./Map.css";
 
 const Map = () => {
   const center = [36.510034987389005, -4.8861989382417566];
 
   return (
-    <div
-      style={{
-        height: "50vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "15%",
-      }}
-    >
+    <div className="map-container">
       <MapContainer
+        className="map"
         center={center}
         zoom={14}
-        style={{ height: "50vh", width: "70%", border: "2px solid grey", borderRadius: "20px" }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={center}>
-          <Popup>Marbella</Popup>
+          <Popup>C. Pedraza, 12, Marbella</Popup>
         </Marker>
       </MapContainer>
     </div>
