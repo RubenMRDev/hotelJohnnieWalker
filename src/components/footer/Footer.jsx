00f61791778551336d7a58e54800415bfb042715
JairoAlexandro <jairosf3113@gmail.com>
2025-02-19 10:56:27 +0100

    Refactor Footer component to use Tailwind CSS and remove unused CSS file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="footer-content">
+    <footer className="relative bottom-0 left-0 w-full text-white bg-[#0c1440] text-center py-2.5">
+      <div className="flex justify-between items-center px-9">
         <p className="hotel-name">&copy; {new Date().getFullYear()} Hotel Johnnie Walker</p>
         <a href="https://github.com/RubenMRDev/hotelJohnnieWalker" target="_blank">
-          <img className="image" src="/github.png" alt="Github logo"></img>
+          <img className="w-9" src="/github.png" alt="Github logo"></img>
         </a>
       </div>
     </footer>
