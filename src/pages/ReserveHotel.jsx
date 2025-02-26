136e72b87e41c0a14c2da80f32b26d74a4d8d081
JairoAlexandro <jairosf3113@gmail.com>
2025-02-26 18:40:58 +0100

    Add RoomCarousel component to ReserveHotel page and clean up package.json

--- src/pages/ReserveHotel.jsx
+++ src/pages/ReserveHotel.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BookHotel from "../components/BookHotel/BookHotel";
+import RoomCarousel from "../components/RoomCarousel/RoomCarousel";
 
 function ReserveHotel() {
 	return (
 		<div>
 			<BookHotel />
+			<RoomCarousel />
 		</div>
 	);
 }
 
-export default ReserveHotel;
 export default ReserveHotel;
